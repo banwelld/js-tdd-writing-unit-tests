041fbd1de64032c8554b32425b64410863509751
Dave Banwell <dave@davebanwell.com>
2024-09-17 21:37:12 -0400

    passes all tests

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,29 @@
-// Your tests here
+import { pointsForWord } from '../utils';
+
+describe('pointsForWord', () => {
+    it('Calculates the total points for a word (1 point per vowel, 2 points per consonant)', () => {
+        const word = 'test';
+        const points = pointsForWord(word);
+        expect(points).toBe(7);
+    });
+    it('Handles words with either uppercase or lowercase letters in any combination', () => {
+        const word = 'ConSonaNt';
+        const points = pointsForWord(word);
+        expect(points).toBe(15);
+    });
+    it('Gives a score of 0 for submissions containing numbers', () => {
+        const word = '123abc';
+        const points = pointsForWord(word);
+        expect(points).toBe(0);
+    });
+    it('Gives a score of 0 for submissions containing special characters', () => {
+        const word = 'abc!@#$';
+        const points = pointsForWord(word);
+        expect(points).toBe(0);
+    });
+    it('Gives a score of 0 for submissions containing spaces', () => {
+        const word = 'abc def';
+        const points = pointsForWord(word);
+        expect(points).toBe(0);
+    });
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,17 @@
-// Your code here
+export function pointsForWord(word) {
+    let points = 0;
+    let eligible = true;
+    const vowels = 'aeiou';
+    const consonants = 'bcdfghjklmnpqrstvwxyz';
+    for (const char of word.toLowerCase()) {
+        if (vowels.includes(char)) {
+            points += 1;
+        } else if (consonants.includes(char)) {
+            points += 2;
+        } else {
+            eligible = false;
+            break;
+        }
+    };
+    return eligible ? points : 0;
+};
